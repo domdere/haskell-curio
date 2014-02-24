73e0b318ea3303b541f364d04e7e3523e3aa92b4
Dom De Re <domdere@domdere.com>
2014-02-25 10:19:13 +1100

    Completed State'' and Store'', should complete Store'

--- src/CT/Monad/State.hs
+++ src/CT/Monad/State.hs
@@ -100,17 +100,78 @@ curry' f x = Func $ \state -> f (Pair state x)
 uncurry' :: (a -> G s b) -> F s a -> b
 uncurry' g (Pair state x) = (runG . g) x state
 
+-- Some (undefined) arrows of interesting types to provide insight into what you can do with curry'/uncurry' and fmap
+
+-- | Try [>> :t curry' fautomorphism] in GHCi
+--
+fautomorphism :: F s a -> F s a
+fautomorphism = undefined
+
+-- | Try [>> :t uncurry' gautomorphism] in GHCi
+--
+gautomorphism :: G s a -> G s a
+gautomorphism = undefined
+
+
 kleisliArrow :: a -> G s (F s b)
 kleisliArrow = undefined
+
+-- by choosing the "canonical" or "obvious" functions for `fautomorphism` and `gautomorphism` and using `curry'` and `uncurry'` (and their properties)
+-- we can derive our units
+
+unit' :: a -> G s (F s a)
+unit' = curry' id
+
+-- | this is the "counit" for `unit'` under the adjunction [F,G]
+--
+counit' :: F s (G s b) -> b
+counit' = uncurry' id
+
+-- | this is technically also a co unit for unit' but under a different adjunction, so we just call it "join" here
+--
+join'' :: G s (F s (G s (F s a))) -> G s (F s a)
+join'' = fmap counit'
+
+duplicate'' :: F s (G s b) -> F s (G s (F s (G s b)))
+duplicate'' = fmap unit'
 
 -- | Now we define the State monad another way...
 -- via the composition G . F
 data State'' s a = State'' { runState'' :: G s (F s a) }
 
+-- | And similarly we can redefine the Store Comonad
+--
+data Store'' s a = Store'' { runStore'' :: F s (G s a)}
+
+-- Just some boiler plate to translate `unit'`, `counit'`, `join'` and `duplicate'`
+-- over to `State''` and `Store''`
+
+unit :: a -> State'' s a
+unit = State'' . unit'
+
+counit :: Store'' s a -> a
+counit = counit' . runStore''
+
+join' :: State'' s (State'' s a) -> State'' s a
+join' = State'' . join'' . runState'' . fmap runState''
+
+duplicate' :: Store'' s a -> Store'' s (Store'' s a)
+duplicate' =  fmap Store'' . Store'' . duplicate'' . runStore''
+
+-- Instances
+
 instance Functor (State'' s) where
     fmap f = State'' . fmap (fmap f) . runState''
 
--- | TODO: Workout how to construct the bind or at least the join...
 instance Monad (State'' s) where
-    return = State'' . curry' id
-    --(State'' ma) >>= (State'' f) = State'' $ (fmap . uncurry' f) ma
+    return = unit
+
+    ma >>= f = (join' . fmap f) ma
+
+instance Functor (Store'' s) where
+    fmap f = Store'' . fmap (fmap f) . runStore''
+
+instance Comonad (Store'' s) where
+    extract = counit
+
+    duplicate = duplicate'
